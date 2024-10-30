101d073f21d67ca58587696d20e0ad2dc0f87719
Kalygylov <Kalydylovzamir666@gmail.com>
2024-10-30 17:26:51 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import '../src/pagesUser/components/header/SupHeader.css'
-
+import '../src/pagesUser/components/footer/Footer.css'
+import Footer from './pagesUser/components/footer/Footer';
+import SupHeader from './pagesUser/components/header/SupHeader';
 function App() {
-	return <div className="App">
-
-  </div>;
+	return (
+    <div>
+      <SupHeader/>
+      <Footer/>
+    </div>
+  )
 }
 
 export default App;
